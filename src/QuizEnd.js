4c304552d4d1bc3e93fccc3849c77c19863e813a
Bryan Dang <bryanvdang@gmail.com>
2019-01-30 20:28:32 -0700

    debugging

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -4,7 +4,7 @@ class QuizEnd extends Component{
   render(){
     return(
       <div>
-        <p>Thanks for Playing</p>
+        <p>Thanks for Playing!</p>
         <a href=''>Reset Quiz</a>
       </div>
     )
